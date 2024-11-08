a87d28eacf6c0f0c95a613baec7cbe6ec9eebaa2
pielakm <s97704@pollub.edu.pl>
2024-11-08 18:45:57 +0100

    edytowanie frontu, niepełne zdarzenie kupna biletu

--- src/models/Models.js
+++ src/models/Models.js
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client"
 
 export const UsersModels = new PrismaClient().users
 export const EventsModels = new PrismaClient().events
+export const EventTicketsModels = new PrismaClient().event_tickets
 export const PaymentMethodModels = new PrismaClient().payment_methods
 export const CommentsModels = new PrismaClient().comments
 export const UserTypesModels = new PrismaClient().user_types
