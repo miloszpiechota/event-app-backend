eae99d43b5c9d312ed1ae90a2f45a66d0813142c
pielakm <s97704@pollub.edu.pl>
2024-11-15 10:29:44 +0100

    dodanie wyszukiwarki + poprawa wyglądu

--- src/routes/EventsRoutes.js
+++ src/routes/EventsRoutes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { EventCreate, EventRead, EventUpdate, EventDelete} from "../controllers/EventsControllers"
+import { EventCreate, EventRead, EventUpdate, EventDelete, EventSearch} from "../controllers/EventsControllers"
 const event_controllers = express.Router()
 import { authCheck } from "../middlewares/AuthCheck"
 import { checkPermission } from "../middlewares/rbacMiddleware"; // Adjust the path as necessary
@@ -11,5 +11,6 @@ event_controllers.get("/read", EventRead)
 event_controllers.put("/update/:id",authCheck, checkPermission('events', 'update'), EventUpdate)
 event_controllers.delete("/delete/:id",authCheck, checkPermission('events', 'delete'), EventDelete)
 event_controllers.get("/read/:id", EventRead);
+event_controllers.get("/search", EventSearch);
 
 export default event_controllers 
